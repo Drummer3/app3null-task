f4e15024e3a2d992a8e274241e08296bb08e968f
Drummer3 <drummerthree@gmail.com>
2022-11-06 12:49:59 +0400

    prepare database migration and user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'country',
+        'birthday',
     ];
 
     /**
@@ -30,6 +32,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
+        'email',
         'remember_token',
     ];
 
