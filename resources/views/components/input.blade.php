8e652785318bfac7062b1191aaedc0a1db15d56d
Drummer3 <drummerthree@gmail.com>
2022-11-07 12:54:14 +0400

    add required to all the inputs

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,7 +1,7 @@
 <?php
 if($type == "select"){
     ?>
-<select class="input-field" name="{{ $name }}">
+<select required class="input-field" name="{{ $name }}">
     <option value="" disabled selected>{{ $placeholder }}</option>
     @foreach ($options as $option)
         <option value="{{ $option }}">{{ $option }}</option>
@@ -10,7 +10,7 @@ if($type == "select"){
 <?php
 } else {
 ?>
-<input class="input-field" name="{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}" />
+<input required class="input-field" name="{{ $name }}" type="{{ $type }}" placeholder="{{ $placeholder }}" />
 <?php
 }
 ?>
