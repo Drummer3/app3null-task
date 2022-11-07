dcc7533813636517b10630b5174dd80dc5c0b888
Drummer3 <drummerthree@gmail.com>
2022-11-07 12:57:41 +0400

    fix typo

--- resources/views/components/user-form.blade.php
+++ resources/views/components/user-form.blade.php
@@ -3,7 +3,7 @@
     <x-input name="name" type="text" placeholder="Name" />
     <x-input name="email" type="email" placeholder="Email" />
     <x-input name="password" type="password" placeholder="Password" />
-    <x-input name="country" type="select" placeholder="Country" :options="['Georgia', 'English']" />
+    <x-input name="country" type="select" placeholder="Country" :options="['Georgia', 'England']" />
     <x-input name="birthday" type="date" placeholder="Date of Birth" />
     <button class="form-submit" type="submit">SAVE</button>
 </form>
